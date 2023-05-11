65454d5c63ed0dab22c3638c29c4293657e66a1e
HugoAlda <Hugoa1204@gmail.com>
2023-05-11 15:54:22 +0200

    v15

--- asdadsa.js
+++
@@ -1,6 +0,0 @@
-const myDiv = document.getElementById('myDiv');
-const expandButton = document.getElementById('expandButton');
-
-expandButton.addEventListener('click', function() {
-  myDiv.classList.toggle('expanded');
-});
